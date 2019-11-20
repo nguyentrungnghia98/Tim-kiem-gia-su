79a8b582590752e975791b1a3a6dcef484ee6f3f
dnnghia98 <mteenzz@gmail.com>
2019-11-21 00:32:19 +0700

    Thêm UI đăng nhập/đăng kí/trang cá nhân

--- admin-front-end/src/index.js
+++ admin-front-end/src/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import Home from './components/index/App';
+import Login from './components/login/LoginView';
+import Register from './components/register/RegisterView';
+import Profile from './components/profile/ProfileView';
+
+export default function App(){
+    return (
+      <Router>
+        <div>
+            <Switch>
+                <Route exact path="/login">
+                    <Login />
+                </Route>
+                <Route path="/register">                    
+                    <Register />
+                </Route>
+                <Route path="/profile">
+                    <Profile />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+             </Switch>
+        </div>
+      </Router>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
