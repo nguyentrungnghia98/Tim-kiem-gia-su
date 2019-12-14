61600ee8346ef5316c87540b4ce461ecf86cbd83
1612421 <nguyenngocnghia53@gmail.com>
2019-12-14 11:24:03 +0700

    Sửa respones của api tạo hợp đồng

--- guest-back-end/routes/contract/contract.js
+++ guest-back-end/routes/contract/contract.js
@@ -24,7 +24,13 @@ router.get('/:id', (req, res) => {
 router.post('/create', CheckUser.passIfIsStudent, async (req, res) => {
     try {
         const contract = await Contract.create(req.userInfo.id, req.body);
-        res.status(200).json({message: "Tạo hợp đồng thành công"});
+        res.status(200).json({
+            results: {
+                object: {
+                    ...contract._doc
+                }
+            }
+        });
     } catch (err) {
         res.status(500).json({message: "Lỗi không xác định được. Thử lại sau"});
     }
